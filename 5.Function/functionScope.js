8075864192761bce309c2b8576ccb5bea7fefefe
Saumya Som <saumyasom92@gmail.com>
2022-09-02 22:27:11 +0530

    Javascript Bootacamp

--- 5.Function/functionScope.js
+++ 5.Function/functionScope.js
@@ -52,7 +52,7 @@ console.log(math.Subtraction(10,2))
 // Higher Order Function = Its mean Accept other fuction as an argument or return as a function
 //Accept other fuction as an argument
 function call(calll){
-  calll() // call the luagh() function twice
+  calll() // call the luagh() function twice... in here call() meaning calling laugh() function
   calll()
 }
 
@@ -63,3 +63,43 @@ function laugh(){
 call(laugh)
 
 //Return as a function
+function multi(num){
+  return function(x){
+    return num * x
+  }
+}
+
+const triple = multi(5) // This call meaning const triple =  function (x) { return 5 * x} 
+console.log(triple(2)); // This call meaning function (x) { return 5 * 2}
+
+// Example :-
+
+function makeBetween (a,b){
+  return function (age){
+    if (age > a && age < b) return true
+    return false
+    
+  }
+}
+
+const isChild = makeBetween(0,18)
+console.log(isChild(15));
+
+// Callback
+// We pass a function to another function as an argument and its execuated..it called callback
+
+//Hoisting ... it problemes with var keywawod
+
+var animal = "Dog"
+console.log(animal); // Result Dog
+
+console.log(newAnimal);
+var newAnimal = "Dog" // Result Undefined
+/*
+so it means when javascipt see var keyword , it will run first..put ont the top. So var animal will run fast, then it will be assigned with value
+
+var animal
+console.log(animal)
+animal = "Dog"
+
+*/
