1d55167b2b2458f1ae9f7deaf9eb475632e91cb7
Saumya Som <saumyasom92@gmail.com>
2022-09-04 21:32:48 +0530

    Javascript Bootcamp

--- 6.Loop Method/arrow.js
+++ 6.Loop Method/arrow.js
@@ -12,4 +12,24 @@ const square1 = x => {return x * x}  // One line if one parameter
 console.log(square(3));
 // Result 9
 
-// Arrow Function Implcait Return
+// Arrow Function Implcait Return.. When a function have single expression then u dont have to write retrun keyword (Optional)
+
+const add = n => n + n
+
+console.log(add(2));
+
+//Example - 
+
+const arr = [1,2,4,5,6,7,8,9]
+
+const parList = arr.map((n)=>{
+  if(n % 2 === 0) return "Even"
+  return "Odd"
+})
+
+console.log(parList);
+
+// One Line
+const parList1 = arr.map((n)=> n % 2 === 0 ?  "Even" : "Odd" )
+
+console.log(parList1);
