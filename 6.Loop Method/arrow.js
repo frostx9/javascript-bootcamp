c4a6dd9c0eef178c8c6ccf1564f00f39fa4f3914
Saumya Som <saumyasom92@gmail.com>
2022-09-05 22:11:08 +0530

    Javascript Bootcamp

--- 6.Loop Method/arrow.js
+++ 6.Loop Method/arrow.js
@@ -1,13 +1,13 @@
 // Arrow Function
 // Syntax - ()=>
 
-const square = (x)=>{
+const square = (x) => {
   return x * x
 }
 
 // Or
 
-const square1 = x => {return x * x}  // One line if one parameter
+const square1 = x => { return x * x }  // One line if one parameter
 
 console.log(square(3));
 // Result 9
@@ -20,16 +20,16 @@ console.log(add(2));
 
 //Example - 
 
-const arr = [1,2,4,5,6,7,8,9]
+const arr = [1, 2, 4, 5, 6, 7, 8, 9]
 
-const parList = arr.map((n)=>{
-  if(n % 2 === 0) return "Even"
+const parList = arr.map((n) => {
+  if (n % 2 === 0) return "Even"
   return "Odd"
 })
 
 console.log(parList);
 
 // One Line
-const parList1 = arr.map((n)=> n % 2 === 0 ?  "Even" : "Odd" )
+const parList1 = arr.map((n) => n % 2 === 0 ? "Even" : "Odd")
 
 console.log(parList1);
