3f109a0a1f627aa8c99753d456732b2ab2148330
Saumya Som <saumyasom92@gmail.com>
2022-09-10 21:19:03 +0530

    Javascript Bootcamp

---
+++ 8.This/computed.js
@@ -0,0 +1,20 @@
+// Compute Propertise
+
+const role = 'host'
+const person = 'Toms Holand'
+
+// const team = {}
+// team[role] = person
+
+const team = {
+  [role]: person,
+  [1 + 6 + 9]: "Sixteen"   // [1 +6 + 9] = evaluate first , and tern into string then it act as a key 
+}
+
+console.log(team);
+
+const addMore = (obj, k, v) => {
+  return { ...obj, [k]: v }
+}
+
+console.log(addMore(team, "Happy", ":)"));
