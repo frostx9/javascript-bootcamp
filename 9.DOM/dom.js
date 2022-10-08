3223b8a4243d92f6335c0ac5a006712b6e3c49c0
Saumya Som <saumyasom92@gmail.com>
2022-10-08 10:23:40 +0530

    Javascript Bootcamp

---
+++ 9.DOM/dom.js
@@ -0,0 +1,20 @@
+// Css Targert
+
+/* 
+To target image from id ..We use '#'
+
+<img id="image"
+    src="https://images.unsplash.com/photo-1665095450911-9b27b929430c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80"
+    alt="default">
+
+#image {
+  width : 250px;
+}
+
+To target image from class ..We use '.'
+
+.special {
+  color : teal
+}
+
+*/
