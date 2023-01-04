8147004d5235b2a5d1e8425618f3f8b36d8f8d68
Saumya Som <saumyasom92@gmail.com>
2023-01-04 23:05:07 +0530

    Javascript Bootcamp

--- Javascript Trick/javascript.js
+++ Javascript Trick/javascript.js
@@ -12,3 +12,20 @@ const itemToBeRemoved = { id: 2, name: 'Dave' }
 apps.splice(apps.findIndex(a => a.id === itemToBeRemoved.id), 1)
 
 console.log(apps) // [ { id: 1, name: 'Jon' }, { id: 3, name: 'Joe' } ]
+
+
+// Update an Object's Property in Array of Objects in JS
+
+const arr2 = [
+  { id: 1, name: 'Alice' },
+  { id: 1, name: 'Bob' },
+  { id: 3, name: 'Charlie' },
+];
+
+for (const obj of arr2) {
+  if (obj.id === 1) {
+    obj.name = 'Alfred';
+
+    break;
+  }
+}
