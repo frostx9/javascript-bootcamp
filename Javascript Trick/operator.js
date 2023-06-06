4d49da6c845adc135ec88e4fd51009ec76b7f1bb
Saumya Som <saumyasom92@gmail.com>
2023-06-06 13:46:34 +0530

    Javascript Bootcamp

--- Javascript Trick/operator.js
+++ Javascript Trick/operator.js
@@ -31,3 +31,42 @@ const vechYearNullish = person.vechile?.year ?? 1970
 console.log(vechYearNullish);
 
 // Basically it says that if left side is undefined the print defaul value as 1970
+
+
+
+
+
+
+
+// --------------------- Nullish coalescing operator (??)-------------------------
+
+const Drive = {
+  Difiulty: "Easy",
+  Starting: 0
+}
+
+
+const cash = Drive.Starting || 500
+
+console.log(cash);
+
+
+/**
+ * Explain -  Suppose There is a game and have starting cash. Above the code we write logic like that if Starting key is not presnt
+ * we use 500 because Drive.Starting make falsy and if present we use Starting cash
+ * 
+ * Now if we set Starting 0 , it also give us 500 because Drive.Starting it make falsy value, but 0 is a proper value based on object logic
+ */
+
+// We can solve two Way
+
+
+// Ternairy operator
+
+const ternary = Drive.Starting === undefined ? 500 : Drive.Starting
+console.log(ternary);
+
+// Nullish Coalescing Operator
+
+const nullish = Drive.Starting ?? 500   // Left Side Check it is value null or undefined. Strict Check
+console.log(nullish);
