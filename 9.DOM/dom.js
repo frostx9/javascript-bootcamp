bd6d77383849eb983f11bdb5b1709aa71e73d01c
Saumya Som <saumyasom92@gmail.com>
2023-06-09 23:32:46 +0530

    Javascript Bootcamp

--- 9.DOM/dom.js
+++ 9.DOM/dom.js
@@ -1,11 +1,15 @@
-// Css Targert
+// Css Targert ----
 
 /*
-To target image from id ..We use '#'
+We can target any element by id and class
+
+To target image from id ..We use -> '#'
 
 <img id="image"
     src="https://images.unsplash.com/photo-1665095450911-9b27b929430c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80"
     alt="default">
+
+Target in Css -> 
 
 #image {
   width : 250px;
@@ -21,8 +25,10 @@ To target image from class ..We use '.'
 
 // Dom - Document Object Model
 //When any web page laods up, brower turn all into bunch of javascript object
+
 /*
 Document is the entry point of DOM. It contains represntaion of all the content of page
+
 it is the height point of html page
           Doucment
              \
