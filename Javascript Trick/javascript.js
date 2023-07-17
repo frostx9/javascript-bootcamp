c20438c3742f1a5ba31937cd4c129dfdd87842ce
Saumya Som <saumyasom92@gmail.com>
2023-07-17 23:33:35 +0530

    Javascript Bootcamp

--- Javascript Trick/javascript.js
+++ Javascript Trick/javascript.js
@@ -217,3 +217,33 @@ accountyArry.forEach((el) => {
     matchAray.push(el)
   }
 })
+
+
+
+const duplicateArray = [
+  {
+    name: 'John',
+    location: 'Los Angeles',
+  },
+  {
+    name: 'Kate',
+    location: 'New York',
+  },
+  {
+    name: 'Mike',
+    location: 'New York',
+  },
+];
+
+const unique = arr.filter(
+  (obj, index) =>
+    arr.findIndex((item) => item.location === obj.location) === index
+);
+
+/*
+   [
+     { name: 'John', location: 'Los Angeles' },
+     { name: 'Kate', location: 'New York' }
+   ]
+ */
+console.log(unique);
