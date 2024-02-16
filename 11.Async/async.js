dfdc6eb4c363601dbeceff01ec9f3f254f3058ea
Saumya Som <saumyasom92@gmail.com>
2024-02-16 11:05:32 +0530

    Async - setTimeout/setInterval/clearTimeout

---
+++ 11.Async/async.js
@@ -0,0 +1,8 @@
+/**
+ * Javascript is - Single Threaded, Synchonos Language (Default)
+ * 
+ * Mean By Default Its Execuation his code line by line at a time
+ * 
+ * Code Write Type = 1. Blocking Code, 2. Non Blocking Code
+ * 
+ */
--- Javascript Behind Scene/execuation.js
+++ Javascript Behind Scene/execuation.js
@@ -1,14 +1,17 @@
 /**
  * Javascipt is Single Threaded Language
- * Javascipt Execuation Obaject
+ * Javascipt Execuation Context
  * 
- * Excuation Context. 1. Global, 2. Fuctional, 3.Eval
  * 
+ * Excuation Context. 1. Global Excuation Context, 2. Fuctional Excuation Context, 3.Eval Excuation Context
+ * 
+ * Phase 1 - Global Execuation
+ * when we write a code, Gloval Excuation Context made
  * {
  * 
  * } => My Code. Gloval Excuation Context. its referst to  'this' . 
  * 
- * Phase 1 - Global Execuation
+ * 
  * 
  * Phase 2 - Memory Creation Phase(MCP). Store Variable
  * 
@@ -20,15 +23,15 @@
  *  let val2 = 5
  *  
  *  function add(num1, num2){
- *    let result = num1 + num2
- *    return result
+ *    let total = num1 + num2
+ *    return total
  *  }
  * 
  *  let result1 = add(val1, val2)
  *  let result2 = add(20, 40)
  * 
  * 
- * Phase 2 => Memory Store Phase. Line 19 to 22 and 27, 28 (Main)
+ * Phase 2 => Memory Store Phase. Line 22, 23, 25, 30, 31
  * 
  * val1 - > Undefiend
  * val2 - > Undefiend
@@ -39,8 +42,8 @@
  * 
  * Phase 3 = > Execaution Phase (Main)
  * 
- * val1 - > 10
- * val2 - > 5
+ * val1 - > 10 .. Store Date
+ * val2 - > 5 .. Store Date
  * 
  * Now When a function call and he create a New Execuation Context.
  * add => Create a New Execuation Context  [ New Variable Environment + Excuation Thread ]. This will every time when add() function call.
@@ -52,7 +55,7 @@
  * 
  * val1 - > Undefiend
  * val2 - > Undefiend
- * result -> Undefiend
+ * total -> Undefiend
  * 
  * Phase 3 = > Execaution Phase
  * 
@@ -87,3 +90,42 @@
  * 
  *  
  */
+
+// Example 1
+function one() {
+  console.log("One")
+
+}
+
+function two() {
+  console.log("Two")
+
+}
+
+function three() {
+  console.log("Three")
+}
+
+one()
+two()
+three()
+
+// Example 2
+function one() {
+  console.log("One")
+  two()
+
+}
+
+function two() {
+  console.log("Two")
+  three()
+}
+
+function three() {
+  console.log("Three")
+}
+
+one()
+two()
+three()
