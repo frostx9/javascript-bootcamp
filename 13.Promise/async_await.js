ff916bfe146792320f05956faeabd29dadbb2ebd
Saumya Som <saumyasom92@gmail.com>
2024-04-17 13:16:34 +0530

    Push Issue Fixed

--- 13.Promise/async_await.js
+++ 13.Promise/async_await.js
@@ -52,27 +52,63 @@ will be print toghter
 
 const p1 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("Promise is Resolved After 10 Sec")
+    resolve("Promise is Resolved After 5 Sec")
   }, 10000)
 })
 
 
 const p2 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("Promise is Resolved after 5 Sec")
+    resolve("Promise is Resolved after 10 Sec")
   }, 5000)
 })
 
 
 async function promiseTwoAsync() {
-  const val = await p1
+
+  console.log("Hello Wold");
+
+  const val = await p1  // In here JS Engine look like w8ing for resolved, but in real scenario is JS is not w8ing for anyone
   console.log("Promise with Async-Await 2");
   console.log(val);
 
   const val1 = await p2
   console.log("Promise with Async-Await 2");
   console.log(val1);
+
+  console.log("Hello Wold 2");
 }
 
 promiseTwoAsync()
 
+/* 
+
+Explanation - P1 = 5 Sec, P2 = 10 Sec
+
+Intailly Call Stack Are empty. Function Execute In Call Stack
+
+when JS execute the promiseTwoAsync() function it will put to the call stack and start to braek the code. "Hello Wold" printed immediately ,
+At that moment when js engine sees await using in the code, js engine remove the promiseTwoAsync() function execuation is suspended and remove 
+from the call stack. So it cant block the main thread because there is only one call stack in engine, and we can not stuck the call stack. other wise page will be freze
+
+so when js engine sees await in line 71. it will suspend the promiseTwoAsync() function execuation for that time and it will w8 for p1 resolved , and when 
+p1 is resolve then only it move ahead
+
+So after 5 sec when p1 is resolve, promiseTwoAsync() function came again into call stack and start execuation from where it left. so it will be 
+line 72 ` console.log("Promise with Async-Await 2")`. So it will log the 72,73 and again it will see the await in line 75. Again suspened the 
+promiseTwoAsync() function. Again Comeback when p2 is resolve. Start the execute from line 76
+
+
+
+Explanation - P1 = 10 Sec, P2 = 5 Sec
+
+Intailly Call Stack Are empty. Function Execute In Call Stack
+
+Same Process. But In here when js saw await in p1 at line 71, it will suspeneded execute the function for 10 sec to promise resolved
+After 10 sec when p1 resolved it start to print
+
+In line 75 again await come in p2. But in that case p2 is already resolved. Because it took only 5 sec. Whcis is less than p1
+so it will print immedate
+
+
+*/
