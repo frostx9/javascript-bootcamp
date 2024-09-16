d172fd8f0fe528ee7d09e254fa23ebb7d12a1ae9
Saumya Som <saumyasom92@gmail.com>
2024-09-16 18:26:46 +0530

    Javascript

--- 4.Loop/while.js
+++ 4.Loop/while.js
@@ -1,9 +1,15 @@
 let j = 0;
 
-while (j <= 5) {
+while (j <= 5) {  // Check Condition First
   console.log(j);
   j++;
 }
+
+/**
+ * Intially J = 0. Loop Condition j <= 5. Mean The Loop is Continue. when J is reach to 5.
+ */
+
+
 
 // when While loop is Great
 const target = Math.floor(Math.random() * 10);
@@ -16,4 +22,14 @@ while (target !== guess) {
 
 console.log(`Target: ${target}, Guess: ${guess}`);
 
+
+
 // Do While
+// Work First. Condtion Checked Latter
+
+let score = 1
+
+do {
+  console.log(`Score: ${score}`);
+  score++
+} while (score <= 5)
